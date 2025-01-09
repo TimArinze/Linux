6e3663678f975d44ad7b1ce680835a283943edaa
Arinze Nwutobo <tim.arinze@outlook.com>
2025-01-09 17:38:15 +0100

    fileio

---
+++ lib/tlpi_hdr.h
@@ -0,0 +1,16 @@
+/* tlpi_hdr.h
+ * Standard header file used by nearly all of the example programs.
+ */
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H		/* Prevent accidental double inclusion */
+
+#include <sys/types.h>		/* Type definitions used by many programs */
+#include <stdio.h>		/* Standard I/O functions */
+#include <stdlib.h>		/* Prototypes of commonly used library functions,
+				   plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h>		/* Prototypes for many system calls */
+#include <errno.h>		/* Declares errno and defines error constants */
+#include <string.h>		/* Commonly used string-handling functions */
+#include <stdbool.h>		/* 'bool' types plus 'true' and 'false' constants */
+
+#endif
